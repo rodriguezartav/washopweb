f9c6d9111f742478f0d7b6bc71f71111bd196586
Roberto Rodriguez <roberto@rodriguezartavia.com>
2020-09-10 07:38:24 -0600

    fc

--- components/Header.js
+++ components/Header.js
@@ -15,27 +15,29 @@ export default class Header extends Component {
                 />
               </a>
             </div>
-            <div className="-mr-2 -my-2 ">
-              <button
-                onClick={this.props.onCartClick}
-                type="button"
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-50 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-              >
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+            {this.props.onCartClick && (
+              <div className="-mr-2 -my-2 ">
+                <button
+                  onClick={this.props.onCartClick}
+                  type="button"
+                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-50 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-              </button>
-            </div>
+                  <svg
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            )}
           </div>
         </div>
 
